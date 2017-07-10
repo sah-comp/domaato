a3f74cc5ae6e56d34bb73b2b00531961a445fd46
Stephan <info@sah-company.com>
2017-07-10 13:12:45 +0200

    Domaato index template has favicons now, dropped email form

--- app/res/tpl/domaato/index_de.php
+++ app/res/tpl/domaato/index_de.php
@@ -20,6 +20,23 @@
 		<meta name="viewport" content="width=device-width">
 
 		<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+        <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png">
+        <link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        <link rel="manifest" href="/manifest.json">
+        <meta name="msapplication-TileColor" content="#ffffff">
+        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+        <meta name="theme-color" content="#ffffff">
         
 		<link rel="stylesheet" href="/css/domaato.css">
 		<!--[if lt IE 9]>
@@ -32,35 +49,9 @@
 		<?php echo Flight::textile(I18n::__('browser_is_ancient')) ?>
 		<![endif]-->
 		<header>
-		    <h1>Domaato</h1>
+		    <h1 class="ir">Domaato</h1>
 		</header>
-		<?php if ( $notification ): ?>
-        <div class="alert alert-<?php echo $notification['class'] ?>">
-            <?php echo $notification['note'] ?>
-        </div>
-		<?php endif ?>
 		<article>
-            <form
-                id="domaato-splash-dialog"
-                class="panel panel-<?php echo $record->getMeta('type') ?>"
-                method="POST"
-                accept-charset="utf-8"
-                enctype="multipart/form-data">
-                <div>
-                    <input type="hidden" name="dialog[type]" value="<?php echo $record->getMeta('type') ?>" />
-                    <input type="hidden" name="dialog[id]" value="<?php echo $record->getId() ?>" />
-                </div>
-                <div class="row">
-		            <input 
-		                type="email" 
-		                name="dialog[email]" 
-		                value="<?php echo $record->email ?>" 
-		                placeholder="<?php echo I18n::__('domaato_placeholder_email_address') ?>"
-		                required="required"
-		                autofocus="autofocus" >
-		            <p class="input-info"><?php echo I18n::__('domaato_info_email_address') ?></p>
-		        </div>
-		    </form>
 		</article>
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 		<script src="/js/domaato.js"></script>
