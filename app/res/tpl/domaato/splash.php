ca30eecc47eb7ac7281d8782c0880891dbef608c
Stephan <info@sah-company.com>
2017-11-23 23:28:15 +0100

    Using real count values and added update of counters

--- app/res/tpl/domaato/splash.php
+++ app/res/tpl/domaato/splash.php
@@ -12,20 +12,25 @@
  * Find three users who have testimonials.
  */
 $_users = R::find( 'user', " testimonial > '' LIMIT 3 " );
+
+/**
+ * Load the initial count information.
+ */
+$_counter = Controller_Api::status( FALSE );
 ?>
 <header>
     <h1 class="brand ir"><a href="<?php echo Url::build( '/' ) ?>" title="<?php echo I18n::__( 'app_name_domaato' ) . ' ' . I18n::__( 'app_claim_domaato' ) ?>">Domaato</a></h1>
 </header>
 <div id="splash" class="animatedParent">
-    
+
     <section id="home" data-tooltip="<?php echo I18n::__( 'domaato_section_home' ) ?>">
         <h1 class="brand ir">Domaato</h1>
         <h2><?php echo I18n::__( 'app_claim_domaato' ) ?></h2>
         <a href="#explore" class="nextSlide animated bounceInUp delay-666"><?php echo I18n::__( 'domaato_ahref_explore' ) ?></a>
     </section>
-    
+
     <section id="testimonials" data-tooltip="<?php echo I18n::__( 'domaato_section_testimonials' ) ?>">
-        
+
 <?php foreach ( $_users as $_id => $_user ): ?>
 
     <div class="slide">
@@ -41,37 +46,37 @@ $_users = R::find( 'user', " testimonial > '' LIMIT 3 " );
         </div>
 
     </div>
-    
+
 <?php endforeach ?>
 
     </section>
 
     <section id="facts" data-tooltip="<?php echo I18n::__( 'domaato_section_facts' ) ?>">
-        
+
         <div class="row">
-            
+
             <div class="span4 counter">
-                <p id="count-report" class="counter" data-target="17500">0</p>
+                <p id="count-report" class="counter" data-target="<?php echo $_counter['count']['report'] ?>">1</p>
                 <p><?php echo I18n::__( 'domaato_counter_reports' ) ?></p>
             </div>
-            
+
             <div class="span4 counter">
-                <p id="count-company" class="counter" data-target="500">0</p>
+                <p id="count-company" class="counter" data-target="<?php echo $_counter['count']['customer'] ?>">1</p>
                 <p><?php echo I18n::__( 'domaato_counter_companies' ) ?></p>
             </div>
-            
+
             <div class="span4 counter">
-                <p id="count-vote" class="counter" data-target="317500">0</p>
-                <p><?php echo I18n::__( 'domaato_counter_votes' ) ?></p>
+                <p id="count-user" class="counter" data-target="<?php echo $_counter['count']['user'] ?>">1</p>
+                <p><?php echo I18n::__( 'domaato_counter_users' ) ?></p>
             </div>
-            
+
         </div>
-        
+
         <div id="newsletter-container" class="newsletter-form">
-            
+
             <h2><?php echo I18n::__( 'domaato_headline_newsletter' ) ?></h2>
             <p><?php echo I18n::__( 'domaato_paragraph_newsletter' ) ?></p>
-            
+
             <form
                 id="newsletter-optin"
                 class="ajaxed"
@@ -97,7 +102,7 @@ $_users = R::find( 'user', " testimonial > '' LIMIT 3 " );
     </section>
 
     <section id="customers" data-tooltip="<?php echo I18n::__( 'domaato_section_customers' ) ?>">
-        
+
         <div class="slide">
             <h2>Apple</h2>
         </div>
@@ -111,45 +116,45 @@ $_users = R::find( 'user', " testimonial > '' LIMIT 3 " );
         </div>
 
     </section>
-    
+
     <section id="contact" data-tooltip="<?php echo I18n::__( 'domaato_section_contact' ) ?>">
-        
+
         <div class="row">
-            
+
             <div class="span4">
-                
+
                 <div class="contact-wrapper">
-                
+
                     <h2><?php echo I18n::__( 'domaato_headline_contact' ) ?></h2>
-                
+
                     <p>
                     Domaato<br />
                     Kochstraße 6<br />
                     34121 Kassel<br />
                     Germany
                     </p>
-                
+
                     <p>
                     Telefon +49 561 123 456 67
                     </p>
-                
+
                     <p>
                     E-Mail <a href="mailto:info@domaato.de">info@domaato.de</a><br />
                     Web <a href="https://domaato.de">https://domaato.de</a>
                     </p>
 
                 </div>
-                
+
             </div>
-            
+
             <div class="span8">
-                
+
                 Contact form or map or whatever.
-                
+
             </div>
-            
+
         </div>
-        
+
     </section>
 
 </div>
--- public/js/domaato-website.js
+++ public/js/domaato-website.js
@@ -9,131 +9,144 @@ var countCounted = false;
  * Options countUp
  */
 var countOptions = {
-    useEasing: true,
-    useGrouping: true,
-    separator: '.',
-    decimal: ','
+  useEasing: true,
+  useGrouping: true,
+  separator: '.',
+  decimal: ','
 };
 
 /* Ready, Set, Go. */
 $(document).ready(function() {
-    
-    /**
-     * Make it a one page website.
-     */
-    $('#splash').fullpage({
-        sectionSelector: 'section',
-		fixedElements: 'header',
-        navigation: true,
-		navigationPosition: 'left',
-		showActiveTooltip: false,
-		slidesNavigation: true,
-		slidesNavPosition: 'bottom',
-		controlArrows: false,
-        afterRender: function () {
-            /**
-             * All links with class nextSlide will scroll one section down.
-             */
-            $('.nextSlide').on('click', function(event) {
-                event.preventDefault();
-                $.fn.fullpage.moveSectionDown();
-                return false;
-            });
-            
-            /**
-             * Clicking the header brand will bring you back to section 1, aka. home.
-             */
-            $('body > header h1 a').on('click', function(event) {
-                event.preventDefault();
-                $.fn.fullpage.moveTo(1);
-                return false;
-            });
-            
-        	/**
-        	 * Form with class inplace will be ajaxified by jQuery form plugin and
-        	 * the response is placed into the element given in data-container.
-        	 */
-            $(document).on( 'submit', '.ajaxed', function(event) {
-                event.preventDefault();
-                var form = $(this);
-                var container = form.data("container");
-                var btn = $( 'input[type=submit]', form);
-                //btn.attr( 'clicked', true );
-                btn.addClass( 'processing' );
-                btn.attr( 'disabled', true );
-                form.ajaxSubmit({
-                    success: function(response) {
-                        btn.removeClass( 'processing' );
-                        //btn.removeAttr( 'clicked' );
-                        btn.removeAttr( 'disabled' );
-                        //alert( 'You have opted in!' );
-                        /*$("#"+container).empty().append(response);*/
-                    }
-                });
-                return false;
-            });
-            
-            /**
-             * Add attribute clicked when clicked.
-             */
-            /*
-            $('form.ajaxed input[type=submit]').on( 'click', function () {
-                $('input[type=submit]', $(this).parents('form')).removeAttr('clicked');
-                $(this).attr('clicked', true);
-            });
-            */
-            
-            /**
-             * Make submit button disabled after clicking it, so form can only be send once.
-             */
-            /*
-            $('form.ajaxed').on( 'submit', function( event ) {
-                var btn = $('input[type=submit][clicked=true]');
-                btn.addClass('processing');
-                btn.attr('disabled', 'disabled');
-                $('.notification').hide();
-                return true;
-            });
-            */
 
-            /**
-             * Init interval for slides and add stopping on mouse over situation.
-             */
-    		pauseSlider = setInterval( function() {
-                $.fn.fullpage.moveSlideRight();
-    		}, sliderInterval);
-    		$('.fp-slides .slide .fp-tableCell').hover(function(ev) {
-    	    	clearInterval(pauseSlider);
-    		}, function(ev) {
-    		    pauseSlider = setInterval( function() {
-    			    $.fn.fullpage.moveSlideRight();
-    		    }, sliderInterval);
-        	});
-        },
-        afterLoad: function(anchorLink, index) {
-            /**
-             * When the first section is active no logo is show in the header.
-             */
-            if ( index == 1 ) {
-                $( 'body > header h1.brand' ).removeClass( 'active' );
-            } else {
-                $( 'body > header h1.brand' ).addClass( 'active' );
-            }
-            
-            /**
-             * On the third section count count has a feast, if not already he counted.
-             */
-            if ( ! countCounted && index == 3 ) {
-                countCounted = true;
-                var countReport = new CountUp( 'count-report', 0, $( '#count-report' ).data( 'target' ), 0, 1, countOptions);
-                countReport.start();
-                var countCompany = new CountUp( 'count-company', 0, $( '#count-company' ).data( 'target' ), 0, 1, countOptions);
-                countCompany.start();
-                var countVote = new CountUp( 'count-vote', 0, $( '#count-vote' ).data( 'target' ), 0, 1, countOptions);
-                countVote.start();
-            }
-            
-        }
-    });
-    
+  /**
+   * Make it a one page website.
+   */
+  $('#splash').fullpage({
+    sectionSelector: 'section',
+    fixedElements: 'header',
+    navigation: true,
+    navigationPosition: 'left',
+    showActiveTooltip: false,
+    slidesNavigation: true,
+    slidesNavPosition: 'bottom',
+    controlArrows: false,
+    afterRender: function() {
+
+      /**
+       * All links with class nextSlide will scroll one section down.
+       */
+      $('.nextSlide').on('click', function(event) {
+        event.preventDefault();
+        $.fn.fullpage.moveSectionDown();
+        return false;
+      });
+
+      /**
+       * Clicking the header brand will bring you back to section 1, aka. home.
+       */
+      $('body > header h1 a').on('click', function(event) {
+        event.preventDefault();
+        $.fn.fullpage.moveTo(1);
+        return false;
+      });
+
+      /**
+       * Form with class inplace will be ajaxified by jQuery form plugin and
+       * the response is placed into the element given in data-container.
+       */
+      $(document).on('submit', '.ajaxed', function(event) {
+        event.preventDefault();
+        var form = $(this);
+        var container = form.data("container");
+        var btn = $('input[type=submit]', form);
+        //btn.attr( 'clicked', true );
+        btn.addClass('processing');
+        btn.attr('disabled', true);
+        form.ajaxSubmit({
+          success: function(response) {
+            btn.removeClass('processing');
+            //btn.removeAttr( 'clicked' );
+            btn.removeAttr('disabled');
+            //alert( 'You have opted in!' );
+            /*$("#"+container).empty().append(response);*/
+          }
+        });
+        return false;
+      });
+
+      /**
+       * Add attribute clicked when clicked.
+       */
+      /*
+      $('form.ajaxed input[type=submit]').on( 'click', function () {
+          $('input[type=submit]', $(this).parents('form')).removeAttr('clicked');
+          $(this).attr('clicked', true);
+      });
+      */
+
+      /**
+       * Make submit button disabled after clicking it, so form can only be send once.
+       */
+      /*
+      $('form.ajaxed').on( 'submit', function( event ) {
+          var btn = $('input[type=submit][clicked=true]');
+          btn.addClass('processing');
+          btn.attr('disabled', 'disabled');
+          $('.notification').hide();
+          return true;
+      });
+      */
+
+      /**
+       * Init interval for slides and add stopping on mouse over situation.
+       */
+      pauseSlider = setInterval(function() {
+        $.fn.fullpage.moveSlideRight();
+      }, sliderInterval);
+      $('.fp-slides .slide .fp-tableCell').hover(function(ev) {
+        clearInterval(pauseSlider);
+      }, function(ev) {
+        pauseSlider = setInterval(function() {
+          $.fn.fullpage.moveSlideRight();
+        }, sliderInterval);
+      });
+    },
+    afterLoad: function(anchorLink, index) {
+      /**
+       * When the first section is active no logo is show in the header.
+       */
+      if (index == 1) {
+        $('body > header h1.brand').removeClass('active');
+      } else {
+        $('body > header h1.brand').addClass('active');
+      }
+
+      /**
+       * On the third section count count has a feast, if not already he counted.
+       */
+      if (!countCounted && index == 3) {
+        countCounted = true;
+        var countReport = new CountUp('count-report', 0, $('#count-report').data('target'), 0, 1, countOptions);
+        countReport.start();
+        var countCompany = new CountUp('count-company', 0, $('#count-company').data('target'), 0, 1, countOptions);
+        countCompany.start();
+        var countUser = new CountUp('count-user', 0, $('#count-user').data('target'), 0, 1, countOptions);
+        countUser.start();
+
+        /**
+         * In a certain interval update the counters in section facts.
+         */
+        var countCountInterval = setInterval(function() {
+          $.get('http://domaato.dev/api/status', function(data) {
+            countReport.update(data.count.report);
+            countCompany.update(data.count.customer);
+            countUser.update(data.count.user);
+          }, 'json');
+        }, 1000);
+
+      }
+
+    }
+  });
+
 });
